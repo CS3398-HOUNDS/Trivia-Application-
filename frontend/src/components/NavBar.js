99c00daa1f4659b54f56b010fd3415cf15137df5
Trevor Chaney <t_c296@txstate.edu>
2020-04-05 13:58:00 -0500

    Reformatted NavBar.js

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -4,20 +4,20 @@ import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 
 class NavBar extends React.Component {
-	render() {
-		return(
-			<Container>
-				<Navbar bg="dark" variant="dark">
-				<Navbar.Brand href="#home">Navbar</Navbar.Brand>
-		    			<Nav className="mr-auto">
-		      				<Nav.Link href="#home">Home</Nav.Link>
-		      				<Nav.Link href="#features">Features</Nav.Link>
-		      				<Nav.Link href="#pricing">Pricing</Nav.Link>
-		    			</Nav>
-				</Navbar>
-			</Container>
-		);
-	}
+  render() {
+    return(
+      <Container>
+        <Navbar bg="dark" variant="dark">
+        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+	  <Nav className="mr-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Navbar>
+      </Container>
+    );
+  }
 }
 
 export default NavBar;

54f0ae5118deac2c92c8b3a900d8379754dcd294
sds158 <34323142+turbophysics@users.noreply.github.com>
2020-04-07 18:11:35 -0500

    Pushing before major changes to css
    The splash looks approxomate to what the intended result will look like so I'm pushing now before changing CSS elements into React Containers

--- frontend/src/components/Splash.js
+++ frontend/src/components/Splash.js
@@ -1,62 +1,30 @@
 import React from "react";
+import Container from "react-bootstrap/Container";
 import '../style.css'
 
 class Splash extends React.Component {
     render() {
         return(
-            <body bgcolor={"#d5f4e6"}>
-            <div style={{maxWidth:"2000px"}}>
-                <div className="w3acontainer w3-content w3-center w3-padding-64" style={{maxWidth: "800"}}>
-                    <div id="firstroot">
-                        <div className="row" style={{
-                            display: "flex",
-                            minWidth: "100%"}}>
-                            <div className="column">
-                                <img src={require("../imgs/2.png")} style={{width:"100%"}} />
-                            </div>
-                            <div className="column">
-                                <img src={require("../imgs/3.png")} style={{width:"100%"}}/>
-                            </div>
-                            <div className="column">
-                                <img src={require("../imgs/4.png")} style={{width:"100%"}}/>
-                            </div>
-                            <div className="column">
-                                <img src={require("../imgs/5.png")} style={{width:"100%"}}/>
-                            </div>
-                            <div className="column">
-                                <img src={require("../imgs/6.png")} style={{width:"100%"}}/>
-                            </div>
-                        </div>
-
-                        <img className={"LetterHead"} src={require("../imgs/LH4.png")} style={{maxWidth:"75%",
-                            height: "auto",
-                            opacity: "1",
-                            padding:"5",
-                            textAlign: "center"
-                        }}/>
-
-                        <div className="row" style={{
-                            display: "flex",
-                            minWidth: "100%"}}>
-                            <div className="column">
-                                <img src={require("../imgs/1.png")} style={{width:"100%"}}/>
-                            </div>
-                            <div className="column">
-                                <img src={require("../imgs/6.png")} style={{width:"100%"}}/>
-                            </div>
-                            <div className="column">
-                                <img src={require("../imgs/3.png")} style={{width:"100%"}}/>
-                            </div>
-                            <div className="column">
-                                <img src={require("../imgs/2.png")} style={{width:"100%"}}/>
-                            </div>
-                            <div className="column">
-                                <img src={require("../imgs/4.png")} style={{width:"100%"}}/>
-                            </div>
-                        </div>
-
-                    </div>
-                </div></div>
+            <body>
+            <div className="splashBody">
+                <div className="splash">
+                    <span className="row " >
+                        <img className="image-column" src={require("../imgs/2.png")} />
+                        <img className="image-column" src={require("../imgs/3.png")} />
+                        <img className="image-column" src={require("../imgs/4.png")} />
+                        <img className="image-column" src={require("../imgs/5.png")} />
+                        <img className="image-column" src={require("../imgs/6.png")} />
+                    </span>
+                    <img className={"letterhead"} src={require("../imgs/LH4.png")}/>
+                    <span className={"row"}>
+                        <img src={require("../imgs/1.png")} />
+                        <img src={require("../imgs/6.png")} />
+                        <img src={require("../imgs/3.png")} />
+                        <img src={require("../imgs/2.png")} />
+                        <img src={require("../imgs/4.png")} />
+                    </span>
+                </div>
+            </div>
             </body>
         );
     }

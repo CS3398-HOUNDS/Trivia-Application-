59542913c886a93fd2791aaa09e328398790c6e1
sds158 <34323142+turbophysics@users.noreply.github.com>
2020-04-08 12:06:24 -0500

    testing git after snafu

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import Nav from 'react-bootstrap/Nav';
 import Splash from './components/Splash';
 import TriviaGame from './components/TriviaGame';
 import Login from './components/Login';
+import CreateGame from './components/CreateGame';
 
 class App extends React.Component {
   constructor(props) {
@@ -29,12 +30,15 @@ class App extends React.Component {
             <Nav.Link onClick={() => this.handleClick("TriviaGame")}>Play</Nav.Link>
             <Nav.Link onClick={() => this.handleClick("Splash")}>Leaderboard</Nav.Link>
             <Nav.Link onClick={() => this.handleClick("Login")}>Login</Nav.Link>
+            <Nav.Link onClick={() => this.handleClick("Create")}>Create</Nav.Link>
+
           </Nav>
         </Navbar>
 
         {userSelection == "Splash" ? <Splash /> :
          userSelection == "Login"  ? <Login />  :
          userSelection == "TriviaGame"  ? <TriviaGame />  :
+             userSelection == "Create"  ? <CreateGame />  :
          <p>The components failed to load</p>}
       </Container>
     );

9ee10843b268b0d289f2e55d61da606b9b9e4e15
Jackson <44758998+ayerswj@users.noreply.github.com>
2020-04-10 15:05:01 -0500

    remove merge conflicts
    removed conflicts that arose when frontend branch was merged with master.

--- backend/core/core/settings.py
+++ backend/core/core/settings.py
@@ -53,20 +53,17 @@ INSTALLED_APPS = [
     'corsheaders',
     'api',
     'djoser',
-    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'core.urls'

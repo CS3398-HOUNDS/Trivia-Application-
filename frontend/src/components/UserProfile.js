5f1da447717b92de27f17d7328658dd6947334e4
sds158 <34323142+turbophysics@users.noreply.github.com>
2020-04-27 21:20:58 -0500

    EditProfile component works
    user profile isn't displaying information before editing it for some reason, however. After the data is edited it works fine

--- frontend/src/components/UserProfile.js
+++ frontend/src/components/UserProfile.js
@@ -1,86 +1,112 @@
 import React from "react";
 import Form from 'react-bootstrap/Form';
+import EditProfile from './EditProfile';
 import Container from 'react-bootstrap/Container';
-import { Row, Col } from 'react-bootstrap';
+import {Row, Col} from 'react-bootstrap';
 import Button from "react-bootstrap/Button";
 
 class UserProfile extends React.Component {
-/* A GET request to /api/v1/profile/{id} will return this information about the user.
-{
-  "user": 0,
-  "bio": "string",
-  "image_url": "string",
-  "location": "string",
-  "birth_date": "2020-04-21",
-  "score": 0,
-  "rank": 0
-}
-*/
-  constructor(props) {
-    super(props);
+    /* A GET request to /api/v1/profile/{id} will return this information about the user.
+    {
+      "user": 0,
+      "bio": "string",
+      "image_url": "string",
+      "location": "string",
+      "birth_date": "2020-04-21",
+      "score": 0,
+      "rank": 0
+    }
+    */
+    constructor(props) {
+        super(props);
 
-    this.state = {
-      username: this.props.name,
-      userIcon: "userIconGoesHere",
-      bio: "No biography supplied.",
-      locale: "No location supplied.",
-      score: -1,
-      rank: -1
-    };
+        this.state = {
+            username: this.props.name,
+            userIcon: "userIconGoesHere",
+            bio: "No biography supplied.",
+            locale: "No location supplied.",
+            score: -1,
+            rank: -1,
+            editing: false
+        };
+        this.getUserInfo();
+        this.getUserInfo = this.getUserInfo.bind(this);
+        this.doneEditing = this.doneEditing.bind(this);
+    }
 
-    this.getUserInfo = this.getUserInfo.bind(this);
-  }
-
-  getUserInfo() {
-    let token = this.props.token;
-    let requestUrl = "http://klingons.pythonanywhere.com/api/v1/profile/" +
-                      this.props.id;
-
-    let response = fetch(requestUrl, {
-      method: "GET",
-      dataType: "JSON",
-      headers: {
-          "Authorization": token,
-      }
-    }).then((resp) => {
-      return resp.json();
-    }).then((resp) => {
+    doneEditing(userData){
       this.setState({
-        bio: resp.bio,
-        userIcon: resp.image_url,
-        locale: resp.location,
-        score: resp.score,
-        rank: resp.rank
-      });
-    }).catch((error) => {
-      console.log(error, "Error in getUserInfo()");
-    })
-  }
+        bio: userData.bio,
+        userIcon: userData.image_url,
+        locale: userData.location,
+        score: userData.score,
+        rank: userData.rank,
+        editing:false
+      })
+    }
 
-  render() {
-    this.getUserInfo();
-    return(
-      <Container style={{backgroundColor: "white"}}>
-        {console.log(this.props)}
-        <Row>
-          <Col>Username: {this.state.username}</Col>
-          <Col>Score: {this.state.score}</Col>
-          <Col>Rank: {this.state.rank}</Col>
-        </Row>
-        <Col>
-          <Row>
-            <img src={this.state.userIcon} />
-          </Row>
-          <Row>
-            <Col>Location: {this.state.local}</Col>
-          </Row>
-        </Col>
-        <Col>
-          <Row md={{ span: 6, offset: 3 }}>Bio: {this.state.bio}</Row>
-        </Col>
-      </Container>
-    );
-  }
+    getUserInfo() {
+        let token = this.props.token;
+        let requestUrl = "http://klingons.pythonanywhere.com/api/v1/profile/" +
+            this.props.id;
+
+        let response = fetch(requestUrl, {
+            method: "GET",
+            dataType: "JSON",
+            headers: {
+                "Authorization": token,
+            }
+        }).then((resp) => {
+            return resp.json();
+        }).then((resp) => {
+            this.setState({
+                bio: resp.bio,
+                userIcon: resp.image_url,
+                locale: resp.location,
+                score: resp.score,
+                rank: resp.rank
+            });
+        }).catch((error) => {
+            console.log(error, "Error in getUserInfo()");
+        })
+    }
+
+    render() {
+
+        return (
+            (!this.state.editing ?
+                    <Container style={{backgroundColor: "white"}}>
+                        <Row>
+                            <Col>Username: {this.state.username}</Col>
+                            <Col>Score: {this.state.score}</Col>
+                            <Col>Rank: {this.state.rank}</Col>
+                        </Row>
+                        <Col>
+                            <Row>
+                                <img src={this.state.userIcon}/>
+                            </Row>
+                            <Row>
+                                <Col>Location: {this.state.local}</Col>
+                            </Row>
+                        </Col>
+                        <Col>
+                            <Row md={{span: 6, offset: 3}}>Bio: {this.state.bio}</Row>
+                        </Col>
+                        <Button onClick={() => {
+                            this.setState({editing: true})
+                        }}>Edit</Button>
+                    </Container>
+                    :
+                    <EditProfile
+                        token={this.props.token}
+                        id={this.props.id}
+                        bio={this.state.bio}
+                        local={this.state.local}
+                        update={this.doneEditing}
+                    />
+            )
+        );
+    }
 }
 
 export default UserProfile;
